a022dd939be2bfe8a9c07a65771cce40a998b597
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2018-03-15 21:21:50 -0700

    add dnn_utils_v2.py

---
+++ jwchen_impl/dnn_utils_v2.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def sigmoid(Z):
+	A = 1/(1+np.exp(-Z))
+	cache = A
+	return A, cache
+
+def sigmoid_backward(dA, cache):
+	A = cache
+	dZ = dA * A * (1 - A)
+	return dZ
+
+def relu(Z):
+	cache = Z
+	A = np.maximum(Z, 0)
+	return A, cache
+
+def relu_backward(dA, cache):
+	Z = cache
+	dZ = Z > 0
+	return dZ
